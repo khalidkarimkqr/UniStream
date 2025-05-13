43d2aff18317db6dbe9feb765ff7e0d5f70ccad6
khalidkarimkqr <khalid13878@hotmail.com>
2025-05-13 01:37:57 -0700

    initialize server with express, cors, dotenv, morgan, and fs

--- server/index.js
+++ server/index.js
@@ -1,1 +1,9 @@
-const express = require("express")
+const express = require("express");
+
+const cors = require("cors")
+
+const dotenv = require ("dotenv")
+
+const {readdirSync} = require("fs")
+
+const morgan = require("morgan")
