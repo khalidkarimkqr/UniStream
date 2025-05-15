c0a3a743318e92dd7cecd210aafdce71bcf27d01
khalidkarimkqr <khalid13878@hotmail.com>
2025-05-14 23:18:21 -0700

    enhance Layout component with react-hot-toast configuration

--- client/src/layouts/Layout.jsx
+++ client/src/layouts/Layout.jsx
@@ -1,7 +1,27 @@
 import React from "react";
 
+import { Toaster } from "react-hot-toast";
+
 const Layout = ({ children }) => {
-  return <div>{children}</div>;
+  return (
+    <>
+      <Toaster
+        position="bottom-center"
+        reverseOrder={false}
+        toastOptions={{
+          style: {},
+          success: {
+            style: {
+              background: "#4caf50",
+              color: "#fff",
+              boxShadow: "",
+            },
+          },
+        }}
+      />
+      <div>{children}</div>
+    </>
+  );
 };
 
 export default Layout;
