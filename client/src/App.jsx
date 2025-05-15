39177b2a9b712d2d0f25cfdfc2e6d742f8a86fe5
khalidkarimkqr <khalid13878@hotmail.com>
2025-05-15 02:41:52 -0700

    hook up AppProvider to make shared state available across the app

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,13 @@
 import React from "react";
 import NavigationRoutes from "./routes/NavigationRoutes";
+import { AppProvider } from "./contexts/AppContext";
 
 function App() {
   return (
     <>
-      <NavigationRoutes />
+      <AppProvider>
+        <NavigationRoutes />
+      </AppProvider>
     </>
   );
 }
---
+++ client/src/contexts/AppContext.jsx
@@ -0,0 +1,31 @@
+import { createContext, useState } from "react";
+
+// eslint-disable-next-line react-refresh/only-export-components
+export const AppContext = createContext(null);
+
+export const AppProvider = ({ children }) => {
+  const [isAuthenticated, setIsAuthenticated] = useState(
+    !!localStorage.getItem("user")
+  );
+
+  const [user, setUser] = useState(
+    JSON.parse(localStorage.getItem("user")) || null
+  );
+
+  const [events, setEvents] = useState([]);
+
+  return (
+    <AppContext.Provider
+      value={{
+        isAuthenticated,
+        setIsAuthenticated,
+        user,
+        setUser,
+        events,
+        setEvents,
+      }}
+    >
+      {children}
+    </AppContext.Provider>
+  );
+};
