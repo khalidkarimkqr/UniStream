543674f3a28d1bcce8d2f096a9b60fc1d6b20484
khalidkarimkqr <khalid13878@hotmail.com>
2025-05-15 04:03:14 -0700

    create reusable Input component with styling and support for props

--- client/src/components/SearchBar.jsx
+++ client/src/components/SearchBar.jsx
@@ -1,7 +1,12 @@
 import React from "react";
+import Input from "./ui/Input";
 
 const SearchBar = () => {
-  return <div>SearchBar</div>;
+  return (
+    <div className="w-full sm:w-[50vw] mx-auto px-5 flex justify-evenly mb-3">
+      <Input />
+    </div>
+  );
 };
 
 export default SearchBar;
--- client/src/components/ui/Input.jsx
+++ client/src/components/ui/Input.jsx
@@ -1,7 +1,17 @@
 import React from "react";
 
-const Input = () => {
-  return <div>Input</div>;
+const Input = ({ type = "text", placeholder, id, onChange }) => {
+  return (
+    <input
+      type={type}
+      name={id}
+      id={id}
+      autoComplete="off"
+      className="py-3 text-center rounded-lg w-full bg-gray-100 focus:outline-none focus:border-amber-300 focus:ring-1 focus:ring-amber-300 transition-all duration-300 ease-in-out text-gray-700 px-5"
+      placeholder={placeholder}
+      onChange={onChange}
+    />
+  );
 };
 
 export default Input;
