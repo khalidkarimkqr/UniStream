f545ea81e203045af0889f936f4897ad1bed2f24
khalidkarimkqr <khalid13878@hotmail.com>
2025-05-14 23:35:45 -0700

    set up route structure with Layout wrapper for Home, Dashboard, Login, and Signup pages

--- client/src/routes/NavigationRoutes.jsx
+++ client/src/routes/NavigationRoutes.jsx
@@ -1,11 +1,47 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "../pages/Home";
+import Dashboard from "../pages/Dashboard";
+import Layout from "../layouts/Layout";
+import Login from "../pages/Login";
+import Signup from "../pages/Signup";
 
 const NavigationRoutes = () => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<h1>About page</h1>} />
+        <Route
+          path="/"
+          element={
+            <Layout>
+              <Home />
+            </Layout>
+          }
+        />
+        <Route
+          path="/dashboard"
+          element={
+            <Layout>
+              <Dashboard />
+            </Layout>
+          }
+        />
+        <Route
+          path="/login"
+          element={
+            <Layout>
+              <Login />
+            </Layout>
+          }
+        />
+        <Route
+          path="/signup"
+          element={
+            <Layout>
+              <Signup />
+            </Layout>
+          }
+        />
       </Routes>
     </BrowserRouter>
   );
