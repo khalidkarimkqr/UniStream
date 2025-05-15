96180d5e80ac1b45c2036b168683620372eb35d1
khalidkarimkqr <khalid13878@hotmail.com>
2025-05-15 04:22:05 -0700

    wire up search input with local state to track user input

--- client/src/components/SearchBar.jsx
+++ client/src/components/SearchBar.jsx
@@ -1,10 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
 import Input from "./ui/Input";
 
 const SearchBar = () => {
+  // eslint-disable-next-line no-unused-vars
+  const [searchTerm, setSearchTerm] = useState("");
+
+  const handleSearch = (e) => {
+    setSearchTerm(e.target.value);
+  };
   return (
     <div className="w-full sm:w-[50vw] mx-auto px-5 flex justify-evenly mb-3">
-      <Input />
+      <Input
+        placeholder="Search for events, clubs or people"
+        id="search"
+        onChange={handleSearch}
+      />
     </div>
   );
 };
