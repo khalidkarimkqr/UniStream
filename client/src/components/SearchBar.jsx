faeb731925edf347a3eaa028bb485cda2dea9494
khalidkarimkqr <khalid13878@hotmail.com>
2025-05-15 16:35:23 -0700

    set up debounced search to fetch events and update global state

--- client/src/components/SearchBar.jsx
+++ client/src/components/SearchBar.jsx
@@ -1,9 +1,27 @@
-import React, { useState } from "react";
+/* eslint-disable no-unused-vars */
+/* eslint-disable react-hooks/exhaustive-deps */
+import React, { useContext, useEffect, useState } from "react";
 import Input from "./ui/Input";
+import axios from "axios";
+import { AppContext } from "../context/AppContext";
 
 const SearchBar = () => {
-  // eslint-disable-next-line no-unused-vars
   const [searchTerm, setSearchTerm] = useState("");
+
+  const { setEvents } = useContext(AppContext);
+
+  useEffect(() => {
+    const debounceTimer = setTimeout(() => {
+      if (searchTerm) {
+        axios
+          .get(
+            `${import.meta.env.VITE_API_URL}/search-event?search=${searchTerm}`
+          )
+          .then((res) => setEvents(res.data.data))
+          .catch((err) => console.log(err));
+      }
+    }, 300);
+  }, [searchTerm]);
 
   const handleSearch = (e) => {
     setSearchTerm(e.target.value);
