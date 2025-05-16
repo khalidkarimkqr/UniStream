17987f07cc51e401b254d0a3ec3b9202c98cecb6
khalidkarimkqr <khalid13878@hotmail.com>
2025-05-16 06:09:46 -0700

    set up initial Room page structure with route params and user context for future video streaming integration

--- client/src/pages/Room.jsx
+++ client/src/pages/Room.jsx
@@ -1,6 +1,10 @@
-import React from "react";
+import React, { useContext } from "react";
+import { useParams } from "react-router-dom";
+import { AppContext } from "../contexts/AppContext";
 
 const Room = () => {
+  const { roomId } = useParams();
+  const { user } = useContext(AppContext);
   return <div>Room</div>;
 };
 
