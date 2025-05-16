7402da0bf309a0a0c4337994c270186e4233b04d
khalidkarimkqr <khalid13878@hotmail.com>
2025-05-15 21:04:22 -0700

    connect login form to backend and update auth state on success

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -5,6 +5,38 @@ import axios from "axios";
 import toast from "react-hot-toast";
 
 const Login = () => {
+  const { setIsAuthenticated } = useContext(AppContext);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      const formData = new FormData(e.target);
+
+      const payload = {
+        email: formData.get("email"),
+        password: formData.get("password"),
+      };
+
+      const res = await axios.post(
+        `${import.meta.env.VITE_API_URL}/login`,
+        payload
+      );
+
+      const data = await res.data;
+
+      if (data.success) {
+        toast.success(data.message);
+        e.target.reset();
+        setIsAuthenticated(true);
+      } else {
+        toast.error(data.message);
+      }
+    } catch (error) {
+      toast.error(error.response.data.message);
+    }
+  };
+
   return (
     <div>
       <form className="sm:w-[50vw] mx-auto px-5 h-[80vh] flex flex-col justify-center gap-4">
