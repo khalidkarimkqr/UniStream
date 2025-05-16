abf0bbd3c65d9fd091f2bf560755b6f29029f901
khalidkarimkqr <khalid13878@hotmail.com>
2025-05-16 02:22:06 -0700

    build out title and description input fields for admin with basic styling and state hookup

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -11,7 +11,14 @@ const Dashboard = () => {
     <div>
       {user?.role === "admin" && (
         <form className="w-full sm:w-[50vw] mx-auto px-5 grid gap-4 my-10">
-          <Input />
+          <Input type="text" placeholder="Title Here" id="title" />
+          <textarea
+            className="py-3 text-center rounded-lg w-full bg-gray-100 focus:outline-none focus:border-amber-300 focus:ring-1 focus:ring-amber-300 transition-all duration-300 ease-in-out text-gray-700 px-5"
+            placeholder="Description Here"
+            id="description"
+            value={description}
+            onChange={(e) => setDescription(e.target.value)}
+          ></textarea>
         </form>
       )}
     </div>
