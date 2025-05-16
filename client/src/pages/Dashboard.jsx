1c99a0b9d8a372818a236eb0a63fae251e9b8ebb
khalidkarimkqr <khalid13878@hotmail.com>
2025-05-16 01:12:41 -0700

    show input form on dashboard only for admin users

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -1,7 +1,21 @@
-import React from "react";
+import React, { useContext, useState } from "react";
+import { AppContext } from "../contexts/AppContext";
+import Input from "../components/ui/Input";
+import Button from "../components/ui/Button";
 
 const Dashboard = () => {
-  return <div>Dashboard</div>;
+  const [description, setDescription] = useState("");
+  const { user } = useContext(AppContext);
+
+  return (
+    <div>
+      {user?.role === "admin" && (
+        <form className="w-full sm:w-[50vw] mx-auto px-5 grid gap-4 my-10">
+          <Input />
+        </form>
+      )}
+    </div>
+  );
 };
 
 export default Dashboard;
