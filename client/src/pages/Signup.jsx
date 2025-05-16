5677fb6452b7dc018b6e12724fa31cf3e0dd7670
khalidkarimkqr <khalid13878@hotmail.com>
2025-05-15 18:59:19 -0700

    start signup form layout with input fields for name and email

--- client/src/pages/Signup.jsx
+++ client/src/pages/Signup.jsx
@@ -1,7 +1,21 @@
 import React from "react";
+import Input from "../components/ui/Input";
 
 const Signup = () => {
-  return <div>Signup</div>;
+  return (
+    <div>
+      <form
+        action=""
+        className="sm:w-[50vw] mx-auto px-5 h-[80vh] flex flex-col justify-center gap-4"
+      >
+        <div className="flex gap-4">
+          <Input id="firstName" type="text" placeholder="First Name Here" />
+          <Input id="lastName" type="text" placeholder="Last Name Here" />
+        </div>
+        <Input id="email" type="email" placeholder="Email Here" />
+      </form>
+    </div>
+  );
 };
 
 export default Signup;
