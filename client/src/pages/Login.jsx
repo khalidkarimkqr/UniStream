066d7fb5a595b26269b5c4e99950c11464c3492b
khalidkarimkqr <khalid13878@hotmail.com>
2025-05-15 20:02:49 -0700

    start login form with email and password input fields

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,7 +1,19 @@
 import React from "react";
+import Input from "../components/ui/Input";
+import Button from "../components/ui/Button";
+import axios from "axios";
+import toast from "react-hot-toast";
 
 const Login = () => {
-  return <div>Login</div>;
+  return (
+    <div>
+      <form>
+        <Input id="email" type="email" placeholder="Email Here" />
+        <Input id="password" type="password" placeholder="Password Here" />
+        <Button text="login" />
+      </form>
+    </div>
+  );
 };
 
 export default Login;
