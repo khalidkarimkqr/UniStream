834467fb3a739d4265fd0e58109570ffa0b11efb
khalidkarimkqr <khalid13878@hotmail.com>
2025-05-17 02:24:58 -0700

    add endLive function to notify backend when host ends event and navigate back to dashboard with toast feedback

--- client/src/pages/Room.jsx
+++ client/src/pages/Room.jsx
@@ -2,11 +2,35 @@ import React, { useContext } from "react";
 import { useNavigate, useParams } from "react-router-dom";
 import { AppContext } from "../contexts/AppContext";
 import { ZegoUIKitPrebuilt } from "@zegocloud/zego-uikit-prebuilt";
+import { toast } from "react-hot-toast";
+import axios from "axios";
 
 const Room = () => {
   const { roomId } = useParams();
   const { user } = useContext(AppContext);
   const navigate = useNavigate();
+
+  const endLive = async () => {
+    try {
+      const res = await axios.put(
+        import.meta.env.VITE_API_URL + `/end-event/${roomId}`,
+        {},
+        {
+          headers: {
+            Authorization: `Bearer ${localStorage.getItem("token")}`,
+          },
+        }
+      );
+
+      const data = await res.data;
+      if (data.success) {
+        navigate("/dashboard");
+        toast.success(data.message);
+      }
+    } catch (error) {
+      toast.error(error.response.data.message);
+    }
+  };
 
   const joinRoom = (element) => {
     const appId = Number(import.meta.env.VITE_ZEGOCLOUD_APP_ID);
