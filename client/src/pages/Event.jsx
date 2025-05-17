9b42c240a3884746675b6e4445d2ebee2fb2f337
khalidkarimkqr <khalid13878@hotmail.com>
2025-05-17 04:24:10 -0700

    set up event detail page with data fetching using eventId and basic layout

--- client/src/pages/Event.jsx
+++ client/src/pages/Event.jsx
@@ -1,7 +1,41 @@
-import React from "react";
+/* eslint-disable react-hooks/exhaustive-deps */
+import React, { useEffect, useContext, useState } from "react";
+import { useParams } from "react-router-dom";
+import { AppContext } from "../contexts/AppContext";
+import axios from "axios";
+import toast from "react-hot-toast";
 
 const Event = () => {
-  return <div>Event</div>;
+  const { eventId } = useParams();
+  const [event, setEvent] = useState({});
+  const { user } = useContext(AppContext);
+
+  useEffect(() => {
+    const getEventById = async () => {
+      const res = await axios.get(
+        import.meta.env.VITE_API_URL + `/get-event-by-id/${eventId}`
+      );
+      const data = await res.data;
+
+      if (data.success) {
+        setEvent(data.data);
+      } else {
+        toast.error(data.message);
+      }
+    };
+    getEventById();
+  }, []);
+
+  return (
+    <div className="w-full sm:w-[50vw] mx-auto px-5 my-10">
+      {event && (
+        <div>
+          <div></div>
+          <div></div>
+        </div>
+      )}
+    </div>
+  );
 };
 
 export default Event;
