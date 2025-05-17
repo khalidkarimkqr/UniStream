5ad906fca798b3c16f16c0b8e280603e8db5fc97
khalidkarimkqr <khalid13878@hotmail.com>
2025-05-17 16:24:00 -0700

    udated the env variables for deployment

--- server/index.js
+++ server/index.js
@@ -32,7 +32,3 @@ readdirSync("./routes").map((route) =>
 );
 
 app.listen(port, () => console.log(`Server is running on port ${port}`));
-
-app.get("/", (req, res) => {
-  res.send("UniStream backend is running 🚀");
-});
